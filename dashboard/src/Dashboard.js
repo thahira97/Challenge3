2d4adae7baf213046d9b5f24eed5b77730d38420
thahira97 <thahirasheerin3@gmail.com>
2023-11-23 11:07:44 -0500

    Add tabs data

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,13 +1,62 @@
-import "./Dashboard.css"
-
+import "./Dashboard.css";
+import Card from "./Card";
 const Dashboard = () => {
+  const tabsData = [
+    {
+      title: "Challenges",
+      content: (
+        <div>
+          <p>
+            You joined PreprLabs on Nov 13, 2023,Here is your challenge journey.{" "}
+          </p>
+          <img src={""} alt="lab-img" style={{ width: "100px" }}></img>
+          <ul>
+            <li>Project Submitted:</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Labs",
+      content: (
+        <div>
+          <p>You joined PreprLabs on Nov 13, 2023,Here is your lab journey. </p>
+          <img src={""} alt="lab-img" style={{ width: "100px" }}></img>
+          <ul>
+            <li>Project Submitted:</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Resources",
+      content: (
+        <div>
+          <p>
+            You joined PreprLabs on Nov 13, 2023,Here is your Resource journey.{" "}
+          </p>
+          <img src={""} alt="lab-img" style={{ width: "100px" }}></img>
+          <button></button>
+        </div>
+      ),
+    },
+  ];
   return (
     <main className="main-container">
       <h1>Welcome Thahira</h1>
-     <section>
-    
-     </section>
+      <section>
+        <Card>
+          <h4>My Progress</h4>
+          <ul className="tabs"></ul>
+        </Card>
+      </section>
     </main>
-  )
-}
-export default Dashboard
+  );
+};
+export default Dashboard;
