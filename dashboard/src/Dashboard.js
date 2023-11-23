e956ed5717b01e21b72fdf8a7a2d6991b345eadf
thahira97 <thahirasheerin3@gmail.com>
2023-11-23 11:14:19 -0500

    Use useState for tabs data

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,5 +1,6 @@
 import "./Dashboard.css";
 import Card from "./Card";
+import { useState } from "react";
 const Dashboard = () => {
   const tabsData = [
     {
@@ -47,13 +48,30 @@ const Dashboard = () => {
       ),
     },
   ];
+ const [ activeTab, setActiveTab ] = useState(0);
+
+ const handleTabClick = (index) => {
+  setActiveTab(index)
+ }
+
   return (
     <main className="main-container">
       <h1>Welcome Thahira</h1>
       <section>
         <Card>
           <h4>My Progress</h4>
-          <ul className="tabs"></ul>
+          <ul className="tabs">
+            {tabsData.map((tab, index ) => (
+              <li
+                key= {index}
+                className= {index === activeTab ? "active" : ""}
+                onClick= {()=> handleTabClick(index)}>
+                 {tab.title}
+              </li>
+            ))
+        
+            }
+          </ul>
         </Card>
       </section>
     </main>
