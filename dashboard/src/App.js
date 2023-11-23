26ea52ec095317cc83857ba8952549e442ff6dce
thahira97 <thahirasheerin3@gmail.com>
2023-11-23 10:47:56 -0500

    Add Dashboard component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Dashboard from './Dashboard';
 import MainNavigation from './MainNavigation';
 
 function App() {
   return (
     <div className="App">
     <MainNavigation></MainNavigation>
+    <Dashboard></Dashboard>
     </div>
   );
 }
---
+++ dashboard/src/Dashboard.js
@@ -0,0 +1,13 @@
+import "./Dashboard.css"
+
+const Dashboard = () => {
+  return (
+    <main className="main-container">
+      <h1>Welcome Thahira</h1>
+     <section>
+
+     </section>
+    </main>
+  )
+}
+export default Dashboard
